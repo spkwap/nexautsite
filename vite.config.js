a8f4c5caed0d350c0a986928ff72b717d6f1d9ef
spkwap <spkw.ania.p@gmail.com>
2025-07-18 19:12:18 +0200

    vite konfiguracja

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        offer: path.resolve(__dirname, 'offer.html'),
+        projects: path.resolve(__dirname, 'projects.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+      }
+    }
   },
   server: {
     port: 3000,
