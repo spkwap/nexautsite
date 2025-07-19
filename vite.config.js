e6dc8a78047035a6496dc5d5d06296fee9330d84
spkwap <spkw.ania.p@gmail.com>
2025-07-19 21:41:08 +0200

    dodana kariera i zmiany w tekscie projekty itp.

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
         about: path.resolve(__dirname, 'about.html'),
         offer: path.resolve(__dirname, 'offer.html'),
         projects: path.resolve(__dirname, 'projects.html'),
+        career: path.resolve(__dirname, 'career.html'),
         contact: path.resolve(__dirname, 'contact.html'),
       }
     }
